c9d1d7dfa359320649453d2f629a1d097e259c75
johnnashwine <johnnashwine@gmail.com>
2026-04-02 18:11:11 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/johnnashmusic/", // This ensures the site loads correctly on GitHub Pages
   server: {
     host: "::",
     port: 8080,
